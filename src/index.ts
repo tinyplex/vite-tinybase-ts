0088ff807d919dd3205132d5797ad85a84305194
James Pearce <github@tripleodeon.com>
2024-07-06 09:53:40 -0700

    v5.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import './index.css';
-import { createStore, ValueOrUndefined } from 'tinybase/debug';
+import { createStore, ValueOrUndefined } from 'tinybase';
 
 // Convenience function for attaching an action to a button
 const onClick = (id: string, onClick: () => void) =>
